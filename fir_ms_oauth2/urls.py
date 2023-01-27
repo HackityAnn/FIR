3c30a1844ee70d1dc262a78f52ff2be33d0d48b0
meult <thomas.meulemans@leaseplan.com>
2023-01-27 13:30:37 +0100

    See if regex paths work with the plugins, not sure how they are loaded yet

--- fir_ms_oauth2/urls.py
+++ fir_ms_oauth2/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import re_path
 
 from fir_ms_oauth2 import views
 
 app_name = 'fir_ms_oauth2'
 
 urlpatterns = [
-    path('ms_auth/home', views.home, name='home'),
-    path('ms_auth/signin', views.sign_in, name='signin'),
-    path('ms_auth/signout', views.sign_out, name='signout'),
-    path('ms_auth/redirect', views.redirect, name='redirect'),
+    re_path(r'^home', views.home, name='home'),
+    re_path(r'^signin', views.sign_in, name='signin'),
+    re_path(r'^signout', views.sign_out, name='signout'),
+    re_path(r'^redirect', views.redirect, name='redirect'),
 ]
 
