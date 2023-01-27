428480a2a5a1ea8bf49890bdcc65381c5f65e03c
meult <thomas.meulemans@leaseplan.com>
2023-01-27 14:33:05 +0100

    Should be post, adding content type to be sure

--- fir_ms_oauth2/ms_graph_helper.py
+++ fir_ms_oauth2/ms_graph_helper.py
@@ -4,13 +4,13 @@ graph_url = 'https://graph.microsoft.com/v1.0'
 
 def get_user(token):
     # Get the user information
-    headers = {'Authorization': f'Bearer {token}'}
+    headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
     params = {'$select': 'userPrincipalName,department,mail'}
     user = requests.get(f'{graph_url}/me', headers=headers, params=params)
     return user.json()
 
 def get_groups(token):
     # Get the groups the user belongs to
-    headers = {'Authorization': f'Bearer {token}'}
-    groups = requests.get(f'{graph_url}/me/getMemberGroups', headers=headers)
+    headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+    groups = requests.post(f'{graph_url}/me/getMemberGroups', headers=headers)
     return groups.json()
