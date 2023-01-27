7766d583bd7947cd49c783cff97c984654a3ce10
meult <thomas.meulemans@leaseplan.com>
2023-01-27 14:23:34 +0100

    Gamble if groups works

--- fir_ms_oauth2/ms_graph_helper.py
+++ fir_ms_oauth2/ms_graph_helper.py
@@ -5,6 +5,6 @@ graph_url = 'https://graph.microsoft.com/v1.0'
 def get_user(token):
     # Get the user information
     headers = {'Authorization': f'Bearer {token}'}
-    params = {'$select': 'userPrincipalName'}
+    params = {'$select': 'userPrincipalName,groups'}
     user = requests.get(f'{graph_url}/me', headers=headers, params=params)
     return user.json()
