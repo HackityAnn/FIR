ce5549c66685def3bc94f3efc598b38326e625eb
meult <thomas.meulemans@leaseplan.com>
2023-03-24 10:31:07 +0100

    Added comments

--- fir/config/base.py
+++ fir/config/base.py
@@ -24,6 +24,8 @@ except ImportError:
         raise RuntimeWarning(tf_error_message)
     TF_INSTALLED = False
 
+# Configuration needed for SSO with Azure AD
+# We purposefully overwrite TF_INSTALLED here as it gives some issues otherwise with possible dual logins
 MS_OAUTH2_INSTALLED = True
 if MS_OAUTH2_INSTALLED:
     TF_INSTALLED = False
