b8ddb275fea2872e63ff310ef79d4312d79ef898
meult <thomas.meulemans@leaseplan.com>
2023-03-30 13:22:41 +0200

    Testing for password non requirements

--- incidents/admin.py
+++ incidents/admin.py
@@ -1,6 +1,7 @@
 from incidents.models import *
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin, get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
@@ -10,8 +11,21 @@ User = get_user_model()
 class ACENestedAdmin(admin.TabularInline):
     model = AccessControlEntry
 
+class PasswordlessUserCreationForm(UserCreationForm):
+    """
+    Class to overwrite UserCreationForm so we
+    can put password to not required
+    for passwordless accounts
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].required = False
+        self.fields['password2'].required = False
+
 
 class UserAdmin(auth_admin.UserAdmin):
+    add_form = PasswordlessUserCreationForm
     inlines = [ACENestedAdmin, ]
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
 
