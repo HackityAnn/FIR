c7b35a3d196753ef4fdad8cadc72ddff2572709e
meult <thomas.meulemans@leaseplan.com>
2023-03-30 15:43:50 +0200

    A forgot to save

--- fir_ms_oauth2/urls.py
+++ fir_ms_oauth2/urls.py
@@ -5,7 +5,6 @@ from fir_ms_oauth2 import views
 app_name = 'fir_ms_oauth2'
 
 urlpatterns = [
-    re_path(r'^home', views.home, name='home'),
     re_path(r'^sign_in', views.sign_in, name='sign_in'),
     re_path(r'^sign_out', views.sign_out, name='sign_out'),
     re_path(r'^redirect', views.redirect, name='redirect'),
